455251ad5cca7f439a84cce3d752b0af15f126c1
Maximilian Beck <maximilianbeck@live.de>
2023-01-20 13:24:18 +0100

    Resnet reproduced, eval mode bug fixed

--- lfmd_notebooks/repo.py
+++ lfmd_notebooks/repo.py
@@ -3,4 +3,4 @@ from ml_utilities.output_loader.repo import Repo
 from omegaconf import OmegaConf
 
 
-REPO = Repo(dir=Path('../../erank'), hydra_defaults=OmegaConf.load('../configs/defaults.yaml'))
+REPO = Repo(dir=Path('../../tflearning'), hydra_defaults=OmegaConf.load('../configs/defaults.yaml'))
